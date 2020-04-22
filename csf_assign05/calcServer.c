7f069b39cda548e75a084c22315e9fd22bd1fa60
Abhi Mohnani <amohnan1@jhu.edu>
2020-04-22 15:32:20 -0400

    Finished assignment 5

---
+++ csf_assign05/calcServer.c
@@ -0,0 +1,82 @@
+#include <stdio.h>      /* for snprintf */
+#include "csapp.h"
+#include "calc.h"
+
+#define LINEBUF_SIZE 1024
+
+int chat_with_client(struct Calc *calc, int infd, int outfd);
+void error(const char *c);
+
+int main(int argc, char **argv) {
+   if (argc != 2) { error("Usage: ./arithserver <port>"); }
+   int server_fd = open_listenfd(argv[1]);
+   if (server_fd < 0) { error("Couldn’t open server socket\n"); }
+   int keep_going = 1;
+
+   struct Calc *calc = calc_create();
+   
+   while (keep_going) {
+     int client_fd = Accept(server_fd, NULL, NULL);
+     if (client_fd > 0) {
+       keep_going = chat_with_client(calc, client_fd, client_fd);
+       close(client_fd); // close the connection
+     }
+   }
+
+   calc_destroy(calc);
+   close(server_fd); // close server socket
+   return 0;
+}
+
+int chat_with_client(struct Calc *calc, int infd, int outfd) {
+        rio_t in;
+        char linebuf[LINEBUF_SIZE];
+
+        /* wrap standard input (which is file descriptor 0) */
+        rio_readinitb(&in, infd);
+
+        /*                                                                      
+         * Read lines of input, evaluate them as calculator expressions,        
+         * and (if evaluation was successful) print the result of each          
+         * expression.  Quit when "quit" command is received.                   
+         */
+        int done = 0;
+        while (!done) {
+	  ssize_t n = rio_readlineb(&in, linebuf, LINEBUF_SIZE);
+                if (n <= 0) {
+		  /* error or end of input */
+		  done = 1;
+                } else if (strcmp(linebuf, "quit\n") == 0 || strcmp(linebuf, "quit\r\n") == 0) {
+		  /* quit command */
+		  done = 1;
+
+		} else if (strcmp(linebuf, "shutdown\n") == 0 ||
+			   strcmp(linebuf, "shutdown\r\n") == 0) {
+		  
+		  return 0;
+		  
+                } else {
+		  /* process input line */
+		  
+		  int result;
+		  if (calc_eval(calc, linebuf, &result) == 0) {
+		    /* expression couldn't be evaluated */
+		    rio_writen(outfd, "Error\n", 6);
+		  } else {
+		    
+		    /* output result */
+		    int len = snprintf(linebuf, LINEBUF_SIZE, "%d\n\
+", result);
+		    if (len < LINEBUF_SIZE) {
+		      rio_writen(outfd, linebuf, len);
+		    }
+		  }
+                }
+        }
+	return 1;
+}
+
+void error(const char *c) {
+  printf("%s\n", c);
+  exit(1);
+}
